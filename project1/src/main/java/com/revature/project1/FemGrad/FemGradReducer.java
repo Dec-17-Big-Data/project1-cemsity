8e540e0d186f3e38e8fbc6470be2b860b1915514
[Stephen Hilson] <[stephenrhilson@gmail.com]>
2019-01-16 15:24:34 -0500

    Added testing for question 1
    MRunit testing for question 1 completed

--- project1/src/main/java/com/revature/project1/FemGrad/FemGradReducer.java
+++ project1/src/main/java/com/revature/project1/FemGrad/FemGradReducer.java
@@ -6,13 +6,19 @@ import org.apache.hadoop.mapreduce.Reducer;
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.Logger;
 
-public class FemGradReducer extends Reducer<Text, Iterable<Text>, Text, Text>{
+public class FemGradReducer extends Reducer<Text, Text, Text, Text>{
 	//private static Logger log = LogManager.getLogger(FemGradReducer.class);
 	public void reduce(Text key, Iterable<Text> values, Context context ) throws IOException, InterruptedException{
 		//log.traceEntry();
 		for (Text val : values) {
-			Text test = new Text("Hello World!");
-			context.write(test, test);
+			String[] strArr = val.toString().split(";");
+			int year = Integer.parseInt(strArr[0]);
+			double value = Double.parseDouble(strArr[1]);
+			if(value < 30.0 && year != -1) {
+				context.write(key, val);
+			} else if (year == -1) {
+				context.write(key, new Text("No Recent Data"));
+			}
 		}
 	}
 }
---
+++ project1/src/test/java/com/revature/project1/FemGradTest.java
@@ -0,0 +1,68 @@
+package com.revature.project1;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.revature.project1.FemGrad.FemGradMapper;
+import com.revature.project1.FemGrad.FemGradReducer;
+
+public class FemGradTest {
+	private MapDriver<LongWritable, Text, Text, Text> mapDriver;
+	private ReduceDriver<Text, Text, Text ,Text> reduceDriver;
+	private MapReduceDriver<LongWritable, Text, Text, Text, Text, Text> mrDriver;
+	
+	
+	@Before
+	public void setUp() {
+		//Mapper
+		FemGradMapper mapper = new FemGradMapper();
+		mapDriver = new MapDriver<LongWritable, Text, Text, Text>();
+		mapDriver.setMapper(mapper);
+		
+		//Reducer
+		
+		FemGradReducer reducer = new FemGradReducer();
+		reduceDriver = new ReduceDriver<Text, Text, Text, Text>();
+		reduceDriver.setReducer(reducer);
+		
+		//MapReduce
+		mrDriver = new MapReduceDriver<LongWritable, Text, Text, Text, Text, Text>();
+		mrDriver.setMapper(mapper);
+		mrDriver.setReducer(reducer);
+	}
+	@Test
+	public void testMapper() {
+		mapDriver.withInput(new LongWritable(1), new Text("\"Egypt, Arab Rep.\",\"EGY\",\"Gross graduation ratio, tertiary, female (%)\",\"SE.TER.CMPL.FE.ZS\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"19.63836\",\"19.53745\",\"21.01584\",\"22.07007\",\"25.87731\",\"25.51352\",\"25.57537\",\"25.8574\",\"23.22869\",\"24.32711\",\"\",\"\",\"25.83895\",\"\",\"\",\"\","));
+	
+		mapDriver.withOutput(new Text("Egypt, Arab Rep."), new Text("2013;25.83895"));
+		
+		mapDriver.runTest();
+	}
+	@Test
+	public void testReducer() {
+		List<Text> value1 = new ArrayList<Text>();
+		value1.add(new Text("2013;25.83895"));
+		
+		reduceDriver.withInput(new Text("Egypt, Arab Rep."), value1);
+		
+		reduceDriver.withOutput(new Text("Egypt, Arab Rep."), new Text("2013;25.83895"));
+		
+		reduceDriver.runTest();
+	}
+	
+	@Test
+	public void testMapReduce() {
+		mrDriver.withInput(new LongWritable(1), new Text("\"Egypt, Arab Rep.\",\"EGY\",\"Gross graduation ratio, tertiary, female (%)\",\"SE.TER.CMPL.FE.ZS\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"19.63836\",\"19.53745\",\"21.01584\",\"22.07007\",\"25.87731\",\"25.51352\",\"25.57537\",\"25.8574\",\"23.22869\",\"24.32711\",\"\",\"\",\"25.83895\",\"\",\"\",\"\","));
+		
+		mrDriver.addOutput(new Text("Egypt, Arab Rep."), new Text("2013;25.83895"));
+		
+		mrDriver.runTest();
+	}
+}
