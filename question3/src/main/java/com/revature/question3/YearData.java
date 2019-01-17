5a06df55e2d7be9a438fb1e1b00223d185c568e4
[Stephen Hilson] <[stephenrhilson@gmail.com]>
2019-01-17 16:04:58 -0500

    Finished Question 3
    Finished coding Question 3. Added a helper class called YearDate.

---
+++ question3/src/main/java/com/revature/question3/YearData.java
@@ -0,0 +1,40 @@
+package com.revature.question3;
+
+public class YearData {
+	private int year;
+	private double data;
+	
+	public YearData(String string) {
+		this.fromString(string);
+	}
+	
+	public YearData(int year, double data) {
+		this.year = year;
+		this.data = data;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getData() {
+		return data;
+	}
+
+	public void setData(double data) {
+		this.data = data;
+	}
+	public String toString() {
+		return "" + this.year + "%%" + this.data;
+	}
+	
+	public void fromString(String string) {
+		String[] arr = string.split("%%");
+		this.year = Integer.parseInt(arr[0]);
+		this.data = Double.parseDouble(arr[0]);		
+	}
+}
